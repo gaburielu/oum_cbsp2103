0a9c7a68acea16244f3c869d900b907dfab972db
Gabriel <gabriel13phoenix@gmail.com>
2025-02-10 21:10:51 +0800

    Adding initial structure, experimenting with function

--- app.c
+++ app.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+struct Event
+{
+    char eventName[50];
+    int eventID;
+    int numTickets;
+    int seats;
+    float price;
+    int totalRevenue;
+};
+
 int main()
 {
+    struct Event shakespear;
+
+    strcpy(shakespear.eventName, "Romeo and Juliet");
+    shakespear.eventID = 20;
+
+    printf("%s\n", shakespear.eventName);
+    printf("%d\n", shakespear.eventID);
 
     return 0;
 }
